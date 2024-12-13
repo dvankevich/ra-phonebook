9e6254d97fb5f616f0208cf35fbed5eb9bc1d5e8
dima <dvankevich@gmail.com>
2024-12-13 15:38:04 +0200

    add NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ import {
   // EditGuesser,
   // ShowGuesser,
 } from "react-admin";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import authProvider from "./authProvider";
 import ContactList from "./ContactList";
 import ContactEdit from "./ContactEdit";
 import ContactCreate from "./ContactCreate";
-import { Dashboard } from './Dashboard';
+import { Dashboard } from "./Dashboard";
+import NotFound from "./NotFound";
 
 export const App = () => (
   // <Admin
@@ -22,19 +23,24 @@ export const App = () => (
   //   dataProvider={dataProvider}
   //   dashboard={Dashboard}
   // >
-    // <Resource name="users" />
-    // <Resource
-    //   name="contacts"
-    //   list={ContactList}
-    //   edit={ContactEdit}
-    //   create={ContactCreate}
-    // />
+  // <Resource name="users" />
+  // <Resource
+  //   name="contacts"
+  //   list={ContactList}
+  //   edit={ContactEdit}
+  //   create={ContactCreate}
+  // />
   // </Admin>
 
-    <Admin layout={Layout} authProvider={authProvider} dataProvider={dataProvider}>
-        <CustomRoutes>
-            <Route path="/" element={<Dashboard />} />
-        </CustomRoutes>
+  <Admin
+    layout={Layout}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <CustomRoutes>
+      <Route path="/404" element={<NotFound />} />
+      <Route path="/" element={<Dashboard />} />
+    </CustomRoutes>
     <Resource name="users" />
     <Resource
       name="contacts"
@@ -42,5 +48,5 @@ export const App = () => (
       edit={ContactEdit}
       create={ContactCreate}
     />
-    </Admin>
+  </Admin>
 );
