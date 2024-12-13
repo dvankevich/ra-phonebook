c27d9abf4e1ffe4f238c5683a5c060e158b25194
dima <dvankevich@gmail.com>
2024-12-13 14:21:52 +0200

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ import authProvider from "./authProvider";
 import ContactList from "./ContactList";
 import ContactEdit from "./ContactEdit";
 import ContactCreate from "./ContactCreate";
+import { Dashboard } from './Dashboard';
 
 export const App = () => (
   <Admin
     layout={Layout}
     authProvider={authProvider}
     dataProvider={dataProvider}
+    dashboard={Dashboard}
   >
     <Resource name="users" />
     <Resource
---
+++ src/Dashboard.tsx
@@ -0,0 +1,8 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+
+export const Dashboard = () => (
+    <Card>
+        <CardHeader title="Welcome to the simple phonebook app." />
+        <CardContent>Your go-to solution for managing contacts effortlessly. Enjoy the benefits of fast contact search and a spacious, user-friendly interface.</CardContent>
+    </Card>
+);
