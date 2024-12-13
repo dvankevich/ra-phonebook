07c8047ce261cfad432a800c1ffd78409f23fb73
dima <dvankevich@gmail.com>
2024-12-13 15:09:10 +0200

    add unautorized route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {
   Admin,
+  CustomRoutes,
   Resource,
   // ListGuesser,
   // EditGuesser,
   // ShowGuesser,
 } from "react-admin";
+import { Route } from 'react-router-dom';
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import authProvider from "./authProvider";
@@ -14,12 +16,25 @@ import ContactCreate from "./ContactCreate";
 import { Dashboard } from './Dashboard';
 
 export const App = () => (
-  <Admin
-    layout={Layout}
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    dashboard={Dashboard}
-  >
+  // <Admin
+  //   layout={Layout}
+  //   authProvider={authProvider}
+  //   dataProvider={dataProvider}
+  //   dashboard={Dashboard}
+  // >
+    // <Resource name="users" />
+    // <Resource
+    //   name="contacts"
+    //   list={ContactList}
+    //   edit={ContactEdit}
+    //   create={ContactCreate}
+    // />
+  // </Admin>
+
+    <Admin layout={Layout} authProvider={authProvider} dataProvider={dataProvider}>
+        <CustomRoutes>
+            <Route path="/" element={<Dashboard />} />
+        </CustomRoutes>
     <Resource name="users" />
     <Resource
       name="contacts"
@@ -27,5 +42,5 @@ export const App = () => (
       edit={ContactEdit}
       create={ContactCreate}
     />
-  </Admin>
+    </Admin>
 );
